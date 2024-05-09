ab12cae6b3241c5892b0f17aec7e7cd695e72814
Stella Mariz Palen <stellamariz.palen@cit.edu>
2024-05-09 09:40:09 +0800

    Make a Function to Concatenate the Values Detected

--- app/src/main/java/com/example/signalink/GestureRecognizerHelper.kt
+++ app/src/main/java/com/example/signalink/GestureRecognizerHelper.kt
@@ -306,7 +306,7 @@ class GestureRecognizerHelper(
 
     companion object {
         val TAG = "GestureRecognizerHelper ${this.hashCode()}"
-        private const val MP_RECOGNIZER_TASK = "phrase.task"
+        private const val MP_RECOGNIZER_TASK = "only_letters.task"
 
         const val DELEGATE_CPU = 0
         const val DELEGATE_GPU = 1
