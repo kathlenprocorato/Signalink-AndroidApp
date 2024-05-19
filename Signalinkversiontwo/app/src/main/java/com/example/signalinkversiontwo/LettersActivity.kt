32a241956e25b0fd22b639536f03900172472e7c
Kathlen Claire Procorato <procorato.kathlenclaire@gmail.com>
2024-05-19 14:03:05 +0800

    Add Letter Dictionary

--- Signalinkversiontwo/app/src/main/java/com/example/signalinkversiontwo/LettersActivity.kt
+++ Signalinkversiontwo/app/src/main/java/com/example/signalinkversiontwo/LettersActivity.kt
@@ -1,6 +1,14 @@
 package com.example.signalinkversiontwo
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class LettersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_letters)
+
+        // Add any additional setup or event listeners here
+    }
 }
