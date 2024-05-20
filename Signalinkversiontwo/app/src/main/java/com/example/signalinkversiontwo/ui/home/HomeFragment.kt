f6bb8573944ef8588c39734c59d123d2f3322b8c
Stella Mariz Palen <stellamariz.palen@cit.edu>
2024-05-20 23:03:09 +0800

    Update UI

--- Signalinkversiontwo/app/src/main/java/com/example/signalinkversiontwo/ui/home/HomeFragment.kt
+++ Signalinkversiontwo/app/src/main/java/com/example/signalinkversiontwo/ui/home/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.signalinkversiontwo.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.signalinkversiontwo.R
 import com.example.signalinkversiontwo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,10 +32,55 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        // Set up button click listener
+        binding.fingerspellingToText.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_permissionsFragment)
+        }
+
+        binding.phraseToText.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_permissionsFragment_to_phrases)
+        }
+
+        val textView: TextView = binding.textWelcome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val textWhatToDoView: TextView = binding.textWhatToDo
+        homeViewModel.whatToDo.observe(viewLifecycleOwner) {
+            textWhatToDoView.text = it
+        }
+
+        val textTranslateHeadingView: TextView = binding.textTranslateHeading
+        homeViewModel.translateHeading.observe(viewLifecycleOwner) {
+            textTranslateHeadingView.text = it
+        }
+
+        val textFingerspelling: TextView = binding.fingerspelling
+        homeViewModel.fingerspelling.observe(viewLifecycleOwner) {
+            textFingerspelling.text = it
+        }
+
+        val textPhrases: TextView = binding.phrases
+        homeViewModel.phrases.observe(viewLifecycleOwner) {
+            textPhrases.text = it
+        }
+
+        val textLearnHeadingView: TextView = binding.textLearnHeading
+        homeViewModel.learnHeading.observe(viewLifecycleOwner) {
+            textLearnHeadingView.text = it
+        }
+
+        val textLearnLetters: TextView = binding.learnLettersText
+        homeViewModel.learnLetters.observe(viewLifecycleOwner) {
+            textLearnLetters.text = it
+        }
+
+        val textTakeQuiz: TextView = binding.takeQuizzesText
+        homeViewModel.takeQuiz.observe(viewLifecycleOwner) {
+            textTakeQuiz.text = it
+        }
+
         return root
     }
 
